c3f5928e8e50d68520679f1c7093b787b9780991
sakshikakde <kakdesakshi@gmail.com>
2021-10-31 01:48:21 -0400

    Implemented class Talker

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,16 +1,26 @@
-#include "ros/ros.h"
-#include "std_msgs/String.h"
-
 /**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
+ * @file talker.cpp
+ * @author Sakshi Kakde
+ * @brief This tutorial demonstrates simple receipt of messages over the ROS system.
+ * @version 0.1
+ * @date 2021-10-31
+ * 
+ * @copyright Copyright (c) 2021
+ * 
  */
-void chatterCallback(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sstream>
+/**
+ * @brief 
+ * 
+ * @param msg 
+ */
+void chatterCallback(const std_msgs::String::ConstPtr& msg) {
+  ROS_INFO("Yes, I heard [%s]", msg->data.c_str());
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
