4d6e18ed4c902d0470b60ba7cd249fe91c23da76
sakshikakde <kakdesakshi@gmail.com>
2021-10-31 02:07:55 -0400

    Called the subscriber

---
+++ src/listener_node.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file listener_node.cpp
+ * @author Sakshi Kakde
+ * @brief Code to subscibe to a topic
+ * @version 0.1
+ * @date 2021-10-31
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#include <beginner_tutorials/listener.hpp>
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "listener_node");
+  Listener listener;
+  listener.runNode();
+  return 0;
+}
